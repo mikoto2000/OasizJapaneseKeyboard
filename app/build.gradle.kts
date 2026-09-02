a4bf109decbe616f5c6b5e4c30d07ad0038fe9bd
mikoto2000 <mikoto2000@gmail.com>
2026-09-02 18:38:08 +0900

    Enable BuildConfig generation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,9 @@ android {
             )
         }
     }
+    buildFeatures {
+        buildConfig = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
